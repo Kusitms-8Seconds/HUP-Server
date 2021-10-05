fc7fcf50d94e430ac21c534c5d636331b4794f0a
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-10-05 22:18:22 +0900

    Initial commit

--- src/main/java/kusitms/AuctionApplication.java
+++ src/main/java/kusitms/AuctionApplication.java
@@ -1,13 +1,22 @@
-package kusitms.auction;
+package kusitms;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class AuctionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
