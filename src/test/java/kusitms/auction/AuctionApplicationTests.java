9a27c473b38968fc04e31de832347ab7654a61e8
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-10-05 20:13:05 +0900

    init commit

---
+++ src/test/java/kusitms/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
