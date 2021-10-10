f8a766604774199ddf67360ac55304f589244085
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-10-11 01:15:51 +0900

    [feat] Add Login Using Spring Security

---
+++ src/main/java/kusitms/domain/user/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package kusitms.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+}
--- src/main/java/kusitms/domain/user/entity/User.java
+++ src/main/java/kusitms/domain/user/entity/User.java
@@ -1,17 +1,18 @@
 package kusitms.domain.user.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
@@ -22,7 +23,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
-    private String name;
+    private String username;
 
     @Column(nullable = false)
     private String password;
@@ -31,13 +32,23 @@ public class User {
 
     private String address;
 
-    @Builder
-    public User(String email, String name, String password, String phoneNumber, String address) {
-        this.email = email;
-        this.name = name;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-    }
+    @Column(name = "activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
+//    @Builder
+//    public User(String email, String username, String password, String phoneNumber, String address) {
+//        this.email = email;
+//        this.username = username;
+//        this.password = password;
+//        this.phoneNumber = phoneNumber;
+//        this.address = address;
+//    }
 
 }
