66b259944da6806735c9b39778d975ddc29d4559
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-11-08 03:01:54 +0900

    feat(HUP-58): ADD NAVER LOGIN OR SIGN UP Validation Using accessToken

--- src/main/java/eightseconds/domain/user/service/OAuth2UserService.java
+++ src/main/java/eightseconds/domain/user/service/OAuth2UserService.java
@@ -4,6 +4,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.nimbusds.jose.shaded.json.parser.ParseException;
 import eightseconds.domain.user.dto.OAuth2GoogleLoginRequest;
 import eightseconds.domain.user.dto.OAuth2KakaoLoginRequest;
+import eightseconds.domain.user.dto.OAuth2NaverLoginRequest;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -12,6 +13,6 @@ import java.security.GeneralSecurityException;
 public interface OAuth2UserService {
     GoogleIdToken validationGoogleIdToken(OAuth2GoogleLoginRequest oAuth2LoginRequest) throws GeneralSecurityException, IOException;
     String saveUserOrUpdateByGoogleIdToken(GoogleIdToken idToken);
-    String validationKakaoCode(OAuth2KakaoLoginRequest oAuth2KakaoLoginRequest) throws IOException, ParseException, org.json.simple.parser.ParseException;
-
+    String validationKakaoAccessToken(OAuth2KakaoLoginRequest oAuth2KakaoLoginRequest) throws IOException, ParseException, org.json.simple.parser.ParseException;
+    String validationNaverAccessToken(OAuth2NaverLoginRequest oAuth2NaverLoginRequest);
 }
