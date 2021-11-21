75b8e375897b4fc75e1b9459d278af9f633e9d0b
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-11-21 12:23:16 +0900

    [feat]: feat getBestItems

---
+++ src/main/java/eightseconds/domain/item/dto/BestItemResponse.java
@@ -0,0 +1,54 @@
+package eightseconds.domain.item.dto;
+
+import eightseconds.domain.file.entity.MyFile;
+import eightseconds.domain.item.constant.ItemConstants;
+import eightseconds.domain.item.entity.Item;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class BestItemResponse {
+
+    private Long id;
+    private String itemName;
+    private ItemConstants.EItemCategory category;
+    private int initPrice;
+    private LocalDateTime buyDate;
+    private int itemStatePoint;
+    private String description;
+    private ItemConstants.EItemSoldStatus soldStatus;
+    private List<String> fileNames;
+    private LocalDateTime auctionClosingDate;
+    private int heart;
+
+    public static BestItemResponse from(Item item, int heart) {
+        List<String> fileNames = new ArrayList<>();
+        if (item.getMyFiles().isEmpty() != true) {
+            List<MyFile> myFiles = item.getMyFiles();
+            fileNames = new ArrayList<>();
+            for (MyFile myFile : myFiles) {
+                fileNames.add(myFile.getFilename());
+            }
+        }
+        return BestItemResponse.builder()
+                .id(item.getId())
+                .itemName(item.getItemName())
+                .category(item.getCategory())
+                .initPrice(item.getInitPrice())
+                .buyDate(item.getBuyDate())
+                .itemStatePoint(item.getItemStatePoint())
+                .description(item.getDescription())
+                .soldStatus(item.getSoldStatus())
+                .auctionClosingDate(item.getAuctionClosingDate())
+                .fileNames(fileNames)
+                .heart(heart)
+                .build();
+    }
+}
--- src/main/java/eightseconds/domain/item/repository/ItemRepository.java
+++ src/main/java/eightseconds/domain/item/repository/ItemRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
@@ -18,4 +19,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(Long itemId);
     @Query("select i from Item i where i.user.id = :userId and i.soldStatus = :soldStatus")
     Page<Item> findAllByStatusAndUserId(Pageable pageable, @Param("soldStatus") EItemSoldStatus soldStatus, @Param("userId") Long userId);
+
+    @Query("select i from Item i where i.soldStatus = :soldStatus")
+    List<Item> findAllItemsByStatus(@Param("soldStatus") EItemSoldStatus soldStatus);
+
 }
