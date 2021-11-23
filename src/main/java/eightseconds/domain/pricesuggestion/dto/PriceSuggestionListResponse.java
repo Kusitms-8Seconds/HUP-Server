2e187189fc38a3678714124e0e7d554d62e7f440
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-11-23 18:46:09 +0900

    [fix]: fix PriceSuggestionListResponse

--- src/main/java/eightseconds/domain/pricesuggestion/dto/PriceSuggestionListResponse.java
+++ src/main/java/eightseconds/domain/pricesuggestion/dto/PriceSuggestionListResponse.java
@@ -1,9 +1,14 @@
 package eightseconds.domain.pricesuggestion.dto;
 
+import eightseconds.domain.file.entity.MyFile;
 import eightseconds.domain.pricesuggestion.entity.PriceSuggestion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,17 +17,33 @@ public class PriceSuggestionListResponse {
 
     private Long priceSuggestionId;
     private Long userId;
+    private String userName;
     private Long itemId;
+    private String itemName;
     private int suggestionPrice;
     private boolean acceptState;
+    private List<String> fileNames;
+    private LocalDateTime auctionClosingDate;
 
     public static PriceSuggestionListResponse from(PriceSuggestion priceSuggestion) {
+        List<String> fileNames = new ArrayList<>();
+        if (priceSuggestion.getItem().getMyFiles().isEmpty() != true) {
+            List<MyFile> myFiles = priceSuggestion.getItem().getMyFiles();
+            fileNames = new ArrayList<>();
+            for (MyFile myFile : myFiles) {
+                fileNames.add(myFile.getFilename());
+            }
+        }
         return PriceSuggestionListResponse.builder()
                 .priceSuggestionId(priceSuggestion.getId())
                 .userId(priceSuggestion.getUser().getId())
+                .userName(priceSuggestion.getItem().getUser().getUsername())
                 .itemId(priceSuggestion.getItem().getId())
+                .itemName(priceSuggestion.getItem().getItemName())
                 .suggestionPrice(priceSuggestion.getSuggestionPrice())
                 .acceptState(priceSuggestion.isAcceptState())
+                .fileNames(fileNames)
+                .auctionClosingDate(priceSuggestion.getItem().getAuctionClosingDate())
                 .build();
     }
 }
