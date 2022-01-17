9a158dcf6775d61abf6f006284dcf4018480fcac
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-17 12:32:14 +0900

    test jenkins

--- src/test/java/eightseconds/hup/HupApplicationTests.java
+++ src/test/java/eightseconds/hup/HupApplicationTests.java
@@ -3,11 +3,11 @@ package eightseconds.hup;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HupApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class HupApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
