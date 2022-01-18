37afb3600e930f00821488c29bc19e7d02032239
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-18 13:50:08 +0900

    [fix]: fix deploy.sh and my.cnf

--- scripts/hup/deploy.sh
+++ scripts/hup/deploy.sh
@@ -8,6 +8,3 @@ sudo docker pull rlawjddn5980/hup-server
 
 # 도커 run
 sudo docker-compose up
-
-# 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
-sudo docker rmi -f $(docker images -f "dangling=true" -q) || true
