12f777c83a82f706d6adf5dd87223eca48cd3891
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-21 15:25:07 +0900

    [delete]: delete the user's item by sales status.

--- src/main/java/eightseconds/domain/item/service/ItemService.java
+++ src/main/java/eightseconds/domain/item/service/ItemService.java
@@ -1,12 +1,8 @@
 package eightseconds.domain.item.service;
 
 import eightseconds.domain.file.entity.MyFile;
-import eightseconds.domain.item.constant.ItemConstants.EItemCategory;
 import eightseconds.domain.item.constant.ItemConstants.EItemSoldStatus;
-import eightseconds.domain.item.dto.BestItemResponse;
-import eightseconds.domain.item.dto.ItemDetailsResponse;
-import eightseconds.domain.item.dto.RegisterItemRequest;
-import eightseconds.domain.item.dto.RegisterItemResponse;
+import eightseconds.domain.item.dto.*;
 import eightseconds.domain.item.entity.Item;
 import eightseconds.global.dto.PaginationDto;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +17,7 @@ public interface ItemService{
     RegisterItemResponse saveItem(Long userId, @Valid RegisterItemRequest registerItemRequest) throws IOException;
     void addFiles(Item item, List<MyFile> saveFiles);
     void deleteByItemId(Long id);
-    void validateItemId(Long id);
+    Item validateItemId(Long id);
     ItemDetailsResponse getItem(Long id);
     Item getItemByItemId(Long id);
     void validateUserAndItem(List<Item> items, Long id);
