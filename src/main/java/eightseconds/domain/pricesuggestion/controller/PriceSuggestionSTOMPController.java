615c7b4d0ff8accd8d99c1fe44abf61f7d5d010e
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-25 00:12:26 +0900

    [feat]: Add Swagger Title to SwaggerConfig

--- src/main/java/eightseconds/domain/pricesuggestion/controller/PriceSuggestionSTOMPController.java
+++ src/main/java/eightseconds/domain/pricesuggestion/controller/PriceSuggestionSTOMPController.java
@@ -8,8 +8,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 @RequiredArgsConstructor
 public class PriceSuggestionSTOMPController {
 
