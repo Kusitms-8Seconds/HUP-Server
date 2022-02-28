68e309080a5efa628253144fb21803f04fa2c690
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-02-28 14:35:44 +0900

    [fix] fix swagger api name of ChatMessageApiController

--- src/main/java/eightseconds/domain/chatmessage/controller/ChatMessageApiController.java
+++ src/main/java/eightseconds/domain/chatmessage/controller/ChatMessageApiController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/chatMessages")
-@Api(tags = "ChatRoom API")
+@Api(tags = "ChatMessage API")
 public class ChatMessageApiController {
 
     private final ChatMessageService chatMessageService;
