26016667530747581724e999b07022f5ea3da7cf
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-03-02 14:03:35 +0900

    [fix] fix application.yml

--- src/main/java/eightseconds/HupApplication.java
+++ src/main/java/eightseconds/HupApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
-import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
