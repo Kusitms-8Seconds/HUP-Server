50144557a8a61385f42bd08d75cf0de08739e60f
kimjungwoo <rlawjddn5980@naver.com>
2022-03-15 01:22:47 +0900

    [feat] Add a date to the alarm message

--- scripts/hup/deploy.sh
+++ scripts/hup/deploy.sh
@@ -1,10 +1,7 @@
 sudo docker-compose down
 
-# 기존 이미지 삭제
 sudo docker rmi rlawjddn5980/hup-server
 
-# 도커허브 이미지 pull
 sudo docker pull rlawjddn5980/hup-server
 
-# 도커 run
 sudo docker-compose up
--- src/main/java/eightseconds/infra/firebase/service/FirebaseCloudMessageService.java
+++ src/main/java/eightseconds/infra/firebase/service/FirebaseCloudMessageService.java
@@ -1,6 +1,5 @@
 package eightseconds.infra.firebase.service;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -12,6 +11,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -21,8 +21,8 @@ public class FirebaseCloudMessageService {
     private final String API_URL = "https://fcm.googleapis.com/v1/projects/auctionapp-f3805/messages:send";
     private final ObjectMapper objectMapper;
 
-    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
-        String message = makeMessage(targetToken, title, body);
+    public void sendMessageTo(String targetToken, String title, String body, LocalDateTime nowLocalDateTime) throws IOException {
+        String message = makeMessage(targetToken, title, body, nowLocalDateTime);
 
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
@@ -33,16 +33,16 @@ public class FirebaseCloudMessageService {
                 .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
                 .build();
         Response response = client.newCall(request).execute();
-        System.out.println(response.body().string());
     }
 
-    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
+    private String makeMessage(String targetToken, String title, String body, LocalDateTime nowLocalDateTime) throws JsonProcessingException {
         FcmMessage fcmMessage = FcmMessage.builder()
                 .message(FcmMessage.Message.builder()
                         .token(targetToken)
                         .notification(FcmMessage.Notification.builder()
                                 .title(title)
                                 .body(body)
+                                .createdDateTime(nowLocalDateTime)
                                 .image("https://firebasestorage.googleapis.com/v0/b/auctionapp-f3805.appspot.com/o/HUPIcon.jpg?alt=media&token=32b7c5b5-59b4-400c-adb8-5994c153d4e3")
                                 .build()
                         ).build()).validateOnly(false).build();
